3f640173233e1ebfcdb8580061fb9fa11ad832d9
Jamie Causey <jcausey@Jamies-MacBook-Air.local>
2021-08-24 09:15:41 -0500

    adding working pipeline

---
+++ jenkins.groovy
@@ -0,0 +1,34 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+            checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/jecausey/s3_testing_jenkins_seed']]])
+            }
+        }
+   
+        stage ('terraform init') {
+            steps {
+                sh ('terraform init')
+            }
+        }
+
+        stage ('terraform Action') {
+            steps {
+                withCredentials([[
+                $class: 'AmazonWebServicesCredentialsBinding',
+                credentialsId: "jenkins_terraform",
+                accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
+                ]])
+                {
+                echo "Terraform action is --> ${action}"
+                /* groovylint-disable-next-line GStringExpressionWithinString */
+                sh ('terraform ${action} --auto-approve')
+            }
+        }
+    }
+}
+}
+
